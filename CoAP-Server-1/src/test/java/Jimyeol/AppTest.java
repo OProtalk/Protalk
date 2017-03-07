5406bc6855f884614958d6890cce164e939a98f5
Nanite Factory <nanite@naver.com>
2017-03-07 18:43:02 +0900

    constructing a coap server project

---
+++ CoAP-Server-1/src/test/java/Jimyeol/AppTest.java
@@ -0,0 +1,36 @@
+package test.java.Jimyeol;
+
+import junit.framework.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
