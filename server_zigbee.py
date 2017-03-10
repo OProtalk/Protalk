930bd15973a7bd54c3d3902659c5e122e0bbadb4
Nanite Factory <nanite@naver.com>
2017-03-10 17:18:19 +0900

    indent with 4 spaces

--- server_zigbee.py
+++ server_zigbee.py
@@ -4,6 +4,6 @@ import time
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 while 1:
-  #print ser.readline()
-  ser.write("1")
-  time.sleep(1000)
+    #print ser.readline()
+    ser.write("1")
+    time.sleep(1000)
