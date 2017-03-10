e6aafc83461df5c1af89dda61f3c142b3b73e603
Nanite Factory <nanite@naver.com>
2017-03-11 05:21:35 +0900

    playing with class CoapServer

--- ServerCoapRd/src/main/java/com/protalk/RdServer.java
+++ ServerCoapRd/src/main/java/com/protalk/RdServer.java
@@ -1,8 +1,10 @@
 package com.protalk;
 
 import java.net.*;
+import java.util.*;
 
 import org.eclipse.californium.core.*;
+import org.eclipse.californium.core.coap.CoAP.*;
 import org.eclipse.californium.core.network.*;
 import org.eclipse.californium.core.network.config.*;
 import org.eclipse.californium.core.server.resources.*;
@@ -12,45 +14,83 @@ public class RdServer extends CoapServer {
 
 	// Constructor for a new Hello-World server initializes the resources of the server.
 	public RdServer() throws SocketException {
-		add(new HelloWorldResource()); // provide an instance of a Hello-World resource
+		add(new HelloResource()); // provide an instance of a Hello-World resource
+		add(new AnotherResource());
 	} // func
 
-	private final class HelloWorldResource extends CoapResource {
-		public HelloWorldResource() {
-			super("helloWorld"); // set resource identifier
-			getAttributes().setTitle("Hello-World Resource"); // set display name
+	// reproductive
+	private final class HelloResource extends CoapResource {
+		
+		private static final String HELLO = "Hello!";
+		private String mGreeting = HELLO;
+		
+		public HelloResource() {
+			super("SayHello"); // 리소스 이름 설정한다. 이 자원에 대한 주소는 이런 식이 될 것이다. coap://[2005::ba27:ebff:fe3c:ba5a]:5683/SayHello
+			getAttributes().setTitle("Greetings Resource"); // 리소스의 title 어트리뷰트를 설정한다. 이 경우 리소스에 대한 링크 서술(link description)은 이런 식이다. </SayHello>;title="Greetings Resource"
 		} // func
 
 		@Override
 		public void handleGET(CoapExchange exchange) {
-			exchange.respond("1. Hello World!"); // respond to the request
-			exchange.respond("2. just another hello world respond");
+			exchange.respond(mGreeting); // 리퀘스트에 대한 응답이다. ACK-2.05 메시지다. respond(String)를 한 번 더 호출하면 CON-2.05를 전송한다.
 		} // func
 
 		@Override
 		public void handlePOST(CoapExchange exchange) {
 			// TODO Auto-generated method stub
-			super.handlePOST(exchange);
-		}		
+			exchange.accept(); // 서버가 반환값을 계산하느라고 메시지 전송이 지연될 수 있을 때, 타임아웃이 일어나지 않게 우선 빈 ack만 보낸다. 진정한 반환 내용은 이 직후에 전송한다.
+			List<String> queries = exchange.getRequestOptions().getUriQuery();
+			//
+			add(new HelloResource());
+			add(new AnotherResource());
+			String res = new String(exchange.getRequestPayload());
+			for(String str : queries) {
+				res += str;
+			}
+			mGreeting += res;
+			//
+			exchange.respond(ResponseCode.CREATED, "greeting set to " + mGreeting);
+		}
 
 		@Override
 		public void handlePUT(CoapExchange exchange) {
 			// TODO Auto-generated method stub
-			super.handlePUT(exchange);
+			//
+			add(new HelloResource());
+			add(new AnotherResource());
+			mGreeting = new String(exchange.getRequestPayload());
+			//
+			exchange.respond(ResponseCode.CHANGED, "greeting set to " + mGreeting);
+			changed(); // notify all observers
 		}
 
 		@Override
 		public void handleDELETE(CoapExchange exchange) {
 			// TODO Auto-generated method stub
-			super.handleDELETE(exchange);
+			// super.handleDELETE(exchange);
+			delete();
+            exchange.respond(ResponseCode.DELETED); // DELETED = 응답 코드 2.02
 		}
 
+	} // class
+	
+	// immature
+	private final class AnotherResource extends CoapResource {
+		public AnotherResource() {
+			super("JustAnotherResouce"); // 리소스 이름 설정한다. 이 자원에 대한 주소는 이런 식이 될 것이다. coap://[2005::ba27:ebff:fe3c:ba5a]:5683/SayHello
+		} // func
+
 		@Override
-		public void handleRequest(Exchange exchange) {
+		public void handleGET(CoapExchange exchange) {
+			exchange.respond("Hey.");
+		} // func
+
+		@Override
+		public void handleDELETE(CoapExchange exchange) {
 			// TODO Auto-generated method stub
-			super.handleRequest(exchange);
+			delete();
+            exchange.respond(ResponseCode.DELETED); 
 		}
-		
+
 	} // class
 
 	// Add individual endpoints listening on default CoAP port on all IPv6 addresses of all network interfaces.
